b1fe9b24493413896a708c1ba135ef07f54057c4
jayeshpal001 <jayashpal5@gmail.com>
2025-03-30 14:19:46 +0530

    some changes in home.jsx

---
+++ src/components/ProjectData.js
@@ -0,0 +1,37 @@
+export let data = [
+    {
+      name: "Ms. Autumn Skiles",
+      email: "katherine60@hotmail.com",
+      address: "81323 Simone Flats Apt. 695\nSouth Lucileville, CO 24991",
+      phone: "+1 (648) 391-8320",
+      website: "https://examplefile.com",
+    },
+    {
+      name: "Mrs. Reba Heathcote",
+      email: "kboyer@reichert.info",
+      address: "13859 Pasquale Mountain Suite 734\nSouth Dayneview, WA 27288",
+      phone: "619.230.2594",
+      website: "https://examplefile.com",
+    },
+    {
+      name: "Prof. Dale Schiller IV",
+      email: "dbrown@hotmail.com",
+      address: "461 Hand Rapid\nLake Angeloville, KS 45943",
+      phone: "(830) 797-0870 x597",
+      website: "https://examplefile.com",
+    },
+    {
+      name: "Miss Leonie Dach",
+      email: "joanie.toy@hackett.com",
+      address: "2182 Brown Route Suite 918\nMariebury, ID 83868",
+      phone: "(753) 618-9594 x983",
+      website: "https://examplefile.com",
+    },
+    {
+      name: "Ruth Stiedemann",
+      email: "wendy.zieme@yahoo.com",
+      address: "762 Powlowski Prairie\nLake Hopestad, WV 47233-0316",
+      phone: "(221) 299-7141 x80069",
+      website: "https://examplefile.com",
+    },
+  ];
