20bbd705ab93bf263804220a4c2de8f943a42121
jayeshpal001 <jayashpal5@gmail.com>
2025-03-30 14:01:49 +0530

    Home page done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Hero from "../assets/hero.png"
+import Hero from "../assets/hero.png";
+
 export const Home = () => {
   return (
     <>
-      <div className="w-screen h-screen">
+      <div className="scroll-smooth w-full min-h-screen">
         <Navbar />
-        <div className="flex justify-between p-20 items-center" >
-          <div className="flex flex-col gap-4">
-            <h1 className="font-bold text-5xl">Accelerate Your Dev Career with Expert Guidance</h1>
-            <p className="text-gray-600">
+        <div className="flex flex-col md:flex-row justify-between p-6 md:p-10 items-center">
+          {/* Left Section */}
+          <div className="flex flex-col gap-5 text-center md:text-left w-full md:w-1/2">
+            <h1 className="font-bold text-3xl md:text-5xl">
+              Accelerate Your Dev Career with Expert Guidance
+            </h1>
+            <p className="text-gray-600 text-lg md:text-2xl">
               Connect with experienced senior developers who can help you level
-              p your skills, navigate career decisions, and grow faster.
+              up your skills, navigate career decisions, and grow faster.
             </p>
-            <button className="bg-black text-white p-3 rounded-lg w-2/8">Find Your Mentor</button>
+            <div className="flex justify-center md:justify-start">
+              <button className="bg-black text-white p-3 rounded-lg w-40 md:w-48">
+                Find Your Mentor
+              </button>
+            </div>
           </div>
-          <img src={Hero} alt="" />
+          {/* Right Section */}
+          <div className="w-full md:w-1/2 flex justify-center mt-6 md:mt-0">
+            <img className="w-3/4 md:w-full max-w-sm md:max-w-2xl" src={Hero} alt="Hero" />
+          </div>
         </div>
       </div>
     </>
